e60d77eba078312374e1890b93f4f5c60332470d
severin <severin.buerki9@gmail.com>
2025-10-28 15:48:44 +0100

    Aufgabe 5

--- src/main/java/exercise5/Radio.java
+++ src/main/java/exercise5/Radio.java
@@ -1,5 +1,37 @@
 package exercise5;
 
 public class Radio {
+    public boolean isOn;
+    public int volume;
+    public double frequenzy;
+    public Radio() {
+        this.isOn = isOn;
+        this.volume = volume;
+        this.frequenzy = frequenzy;
+    }
+    public void on() {
+        if (isOn == true) {
+            System.out.println("Radio is on");
+        }
+    }
+    public void off() {
+        if (isOn == false) {
+            System.out.println("Radio is off");
+        }
+    }
+    public void increaseVolume() {
+        this.volume++;
+    }
+    public void decreaseVolume() {
+        this.volume--;
+    }
+    public void selectChannel(double frequenzy){
+        this.frequenzy = frequenzy;
 
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
--- src/test/java/exercise5/RadioTest.java
+++ src/test/java/exercise5/RadioTest.java
@@ -1,7 +1,53 @@
 package exercise5;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
 
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void on() {
+        boolean isON = true;
+        assertEquals(true, isON);
+    }
+
+    @Test
+    void off() {
+        boolean isOFF = true;
+        assertEquals(true, isOFF);
+    }
+
+    @Test
+    void increaseVolume() {
+        int increaseVolume = 10;
+        assertEquals(10, increaseVolume);
+    }
+
+    @Test
+    void decreaseVolume() {
+        int decreaseVolume = 5;
+        assertEquals(5, decreaseVolume);
+    }
+
+    @Test
+    void selectChannel() {
+        double selectChannel = 4.2;
+        assertEquals(4.2, selectChannel);
+    }
+
+    @Test
+    void testToString() {
+        String toString = "radio";
+    }
 }
