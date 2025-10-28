c018f59976a62a3d6fae9804051f739d8cde39f1
severin <severin.buerki9@gmail.com>
2025-10-28 09:34:26 +0100

    Aufgabe 1 und 2

--- src/main/java/exercise2/Main.java
+++ src/main/java/exercise2/Main.java
@@ -2,6 +2,8 @@ package exercise2;
 
 public class Main {
     public static void main(String[] args) {
-        //Main Method
+        Patient Matteo = new Patient("Matteo Cottschentronne", 67, 67, "male" , true);
+        System.out.println(Matteo);
     }
+
 }
--- src/main/java/exercise2/Patient.java
+++ src/main/java/exercise2/Patient.java
@@ -2,12 +2,64 @@ package exercise2;
 
 public class Patient {
 
-    //TODO: Declare Private Attributes
+    private String name;
+    private int age;
+    private double weight;
+    private String gender;
+    private boolean married;
 
-    //TODO: Create Constructor
 
-    //TODO: Add Getter and Setter Methods
+    public Patient(String name, int age, double weight, String gender, boolean married) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.gender = gender;
+        this.married = married;
 
-    //TODO: Add toString Method
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+
+    @Override
+    public String toString() {
+        return name + " age: " + age + " weight: " + weight + " gender: " + gender + " married: " + married;
+    }
 }
