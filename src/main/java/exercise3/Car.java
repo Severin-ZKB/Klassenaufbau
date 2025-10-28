73377ffdcd48205ec18831512e6361135c2c6ed3
severin <severin.buerki9@gmail.com>
2025-10-28 11:57:02 +0100

    Aufgabe 3

--- src/main/java/exercise3/Car.java
+++ src/main/java/exercise3/Car.java
@@ -1,26 +1,64 @@
 package exercise3;
 
 public class Car {
-  public int fuelLevel;
-  public int mileage;
+  public double fuelLevel;
+  public double mileage;
   public String licensePlate;
-  public String fuelConsumption;
-  public String fuelCapacity;
+  public double fuelConsumption;
+  public int fuelCapacity;
 
-  public Car(String zh420, int i, int i1, double v, int i2) {
+  public Car(String licensePlate, int mileage, int fuelCapacity, double fuelConsumption, int fuelLevel) {
+      this.licensePlate = licensePlate;
+      this.mileage = mileage;
+      this.fuelLevel = fuelLevel;
+      this.fuelConsumption = fuelConsumption;
+      this.fuelCapacity = fuelCapacity;
+
   }
 
-  //declare vars
 
-  //Create constructor
+  public double getFuelLevel(){
+      return this.fuelLevel;
+  }
+  public double getMileage(){
+      return this.mileage;
+  }
+  public String getLicensePlate(){
+      return this.licensePlate;
+  }
+  public double getFuelConsumption(){
+      return this.fuelConsumption;
+  }
+  public int  getFuelCapacity(){
+      return this.fuelCapacity;
+  }
 
   public void drive(int distance) {
-    //do stuff
+      mileage += distance;
+      fuelLevel -= fuelConsumption * distance;
+      double reichweiteTank = distance * fuelConsumption;
+      if (fuelLevel - reichweiteTank < 0) {
+          double maxDistance = fuelLevel / fuelConsumption;
+          mileage += (double) Math.round(maxDistance * 100) / 100;
+          fuelLevel = 0.0;
+      }
+
+
+
+
   }
 
   public void refuel(int amount) {
-    //do stuff
+  fuelLevel = fuelCapacity;
+      while (fuelLevel > 0 && fuelConsumption > fuelLevel) {
+          fuelLevel = fuelCapacity;
+
+
+
+      }
   }
 
-  //add toString
+  public String toString() {
+      return "Car{licensePlate='" + licensePlate + "', mileage="+mileage+", fuelConsumption="+fuelConsumption+", fuelCapacity="+fuelCapacity+ ", fuelLevel="+fuelLevel+"}";
+  }
 }
